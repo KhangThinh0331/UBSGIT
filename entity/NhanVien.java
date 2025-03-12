ce528a82f339d1647833580eccacedf47c095ce0
KhangThinh0331 <khangthinh0331@gmail.com>
2025-03-12 22:23:48 +0700

    add file

---
+++ entity/NhanVien.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bmtcs.entity;
+
+/**
+ *
+ * @author Ca Ngao
+ */
+public class NhanVien {
+    private String maNV;
+    private String SDT;
+    private String vaiTro;
+    private String matKhau;
+    private String hoTen;
+    private String trangThai;
+}
